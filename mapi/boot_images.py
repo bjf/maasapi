bfc3c423241347eef70b04e525a4711050ecf252
Brad Figg <brad.figg@canonical.com>
2015-08-13 12:24:02 -0700

    [mapi/boot_image(s).py] Implementation for /nodegroups/{uuid}/boot-images
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

---
+++ mapi/boot_images.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+#
+
+from log                                import center, cleave, cdebug
+from boot_image                         import BootImage
+from error                              import MapiError
+
+# BootImages
+#
+class BootImages(object):
+    '''
+    '''
+
+    # __init__
+    #
+    def __init__(s, maas, uuid):
+        center(s.__class__.__name__)
+        s.__maas = maas
+        s.__uuid = uuid
+        s.__boot_images = None
+        cleave(s.__class__.__name__)
+
+    # __len__
+    #
+    def __len__(s):
+        return len(list(s.__iter__()))
+
+    # __getitem__
+    #
+    def __getitem__(s, index):
+        center(s.__class__.__name__)
+        s.__fetch_if_needed()
+        retval = BootImage(s.__maas, s.__boot_images[index])
+        cleave(s.__class__.__name__)
+        return retval
+
+    # __iter__
+    #
+    def __iter__(s):
+        center(s.__class__.__name__)
+        s.__fetch_if_needed()
+        for ptype in s.__boot_images:
+            n = BootImage(s.__maas, ptype)
+            yield n
+        cleave(s.__class__.__name__)
+
+    # __fetch_if_needed
+    #
+    def __fetch_if_needed(s):
+        center(s.__class__.__name__)
+        if s.__boot_images is None:
+            response = s.__maas._get(u'/nodegroups/%s/boot-images/' % s.__uuid)
+            if not response.ok:
+                if type(response.data) == str:
+                    cleave(s.__class__.__name__)
+                    raise MapiError(response.data)
+
+            s.__boot_images = response.data
+            cdebug('    fetched')
+        cleave(s.__class__.__name__)
+
