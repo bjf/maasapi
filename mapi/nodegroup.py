a08851c4bfeef14ad2a07ac31918886061851595
Brad Figg <brad.figg@canonical.com>
2015-08-13 09:33:48 -0700

    [nodegroup.py/power_type.py] Don't fake a dict, be a dict.
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- mapi/nodegroup.py
+++ mapi/nodegroup.py
@@ -2,10 +2,11 @@
 #
 
 from log                                import center, cleave
+from copy                               import deepcopy
 
 # Nodegroup
 #
-class Nodegroup(object):
+class Nodegroup(dict):
     '''
     '''
 
@@ -14,8 +15,22 @@ class Nodegroup(object):
     def __init__(s, maas, group):
         center('Nodegroup.__init__')
         s.__maas = maas
-        s.__group = group
-        for k in group:
-            setattr(s, k, group[k])
+
+        dict.__init__(s, group)
+
         cleave('Nodegroup.__init__')
+
+    # interfaces
+    #
+    @property
+    def interfaces(s):
+        center('Nodegroup.interfaces')
+
+        # nodegroups interfaces ---
+        _url = u'/nodegroups/%s/interfaces/' % s['uuid']
+        response = s.__maas._get(_url, op='list')
+        retval = response.data
+
+        center('Nodegroup.interfaces')
+        return retval
 
--- mapi/power_type.py
+++ mapi/power_type.py
@@ -6,7 +6,7 @@ from copy                               import deepcopy
 
 # PowerType
 #
-class PowerType(object):
+class PowerType(dict):
     '''
     '''
 
@@ -15,33 +15,8 @@ class PowerType(object):
     def __init__(s, maas, ptype):
         center('PowerType.__init__')
         s.__maas = maas
-        s.__power_type = ptype
 
-        s._dict = dict()
-        s._dict = deepcopy(ptype)
+        dict.__init__(s, ptype)
+
         cleave('PowerType.__init__')
 
-    # __len__
-    #
-    def __len__(s):
-        return len(list(s._dict.__iter__()))
-
-    # __getitem__
-    #
-    def __getitem__(s, index):
-        center('PowerType.__getitem__')
-        retval = s._dict[index]
-        cleave('PowerType.__getitem__')
-        return retval
-
-    # __iter__
-    #
-    def __iter__(s):
-        center('PowerType.__iter__')
-        retval = s._dict.__iter__()
-        cleave('PowerType.__iter__')
-
-    # __str__
-    #
-    def __str__(s):
-        return str(s._dict)
