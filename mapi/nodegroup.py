193ab76c47d62fa639b08d5c3c569abf2cde8e2f
Brad Figg <brad.figg@canonical.com>
2015-08-14 09:49:02 -0700

    [nodegroup.py] Initial work adding import_boot_images, nodes and details methods
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- mapi/nodegroup.py
+++ mapi/nodegroup.py
@@ -4,6 +4,7 @@
 from log                                import center, cleave
 from interfaces                         import Interfaces
 from boot_images                        import BootImages
+from error                              import MapiError
 
 # Nodegroup
 #
@@ -25,6 +26,9 @@ class Nodegroup(dict):
     #
     @property
     def interfaces(s):
+        '''
+        A collection of all of the network interfaces for a particular nodegroup.
+        '''
         center('Nodegroup.interfaces')
 
         retval = Interfaces(s.__maas, s['uuid'])
@@ -36,9 +40,82 @@ class Nodegroup(dict):
     #
     @property
     def boot_images(s):
+        '''
+        A collection of all of the boot imags for a particular nodegroup.
+        '''
         center(s.__class__.__name__)
 
         retval = BootImages(s.__maas, s['uuid'])
 
         cleave(s.__class__.__name__)
         return retval
+
+    # nodes
+    #
+    @property
+    def nodes(s):
+        '''
+        A collection of all of the nodes in a particular nodegroup.
+        '''
+        center(s.__class__.__name__)
+
+        # Will probably produce a Nodes object but for now just return what comes
+        # back from the rest interface.
+        #
+        response = s.__maas._get(u'/nodegroups/%s/' % s['uuid'], op='list_nodes')
+        if not response.ok:
+            if type(response.data) == str:
+                cleave(s.__class__.__name__)
+                raise MapiError(response.data)
+
+        retval = response.data
+
+        cleave(s.__class__.__name__)
+        return retval
+
+    # details
+    #
+    @property
+    def details(s):
+        '''
+        A collection of detailed information about all of the nodes
+        in a particular nodegroup.
+        '''
+        center(s.__class__.__name__)
+
+        # Will probably produce a Nodes object but for now just return what comes
+        # back from the rest interface.
+        #
+        response = s.__maas._post(u'/nodegroups/%s/' % s['uuid'], op='details')
+        if not response.ok:
+            if type(response.data) == str:
+                cleave(s.__class__.__name__)
+                raise MapiError(response.data)
+
+        retval = response.data
+
+        cleave(s.__class__.__name__)
+        return retval
+
+    # import_boot_images
+    #
+    def import_boot_images(s):
+        '''
+        Import the boot image files for a particular nodegroup.
+        '''
+        center(s.__class__.__name__)
+
+        # Will probably produce a Nodes object but for now just return what comes
+        # back from the rest interface.
+        #
+        response = s.__maas._post(u'/nodegroups/%s/' % s['uuid'], op='import_boot_images')
+        if not response.ok:
+            if type(response.data) == str:
+                cleave(s.__class__.__name__)
+                raise MapiError(response.data)
+
+        retval = response.data
+
+        cleave(s.__class__.__name__)
+        return retval
+
