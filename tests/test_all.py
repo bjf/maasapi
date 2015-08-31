f1a3e3f24c43571db8273d7cdad7e377e0042743
Brad Figg <brad.figg@canonical.com>
2015-08-31 11:12:43 -0700

    [tests/test_all.py] A simple way to run all the test cases
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

---
+++ tests/test_all.py
@@ -0,0 +1,7 @@
+
+import unittest
+from test_tags                          import TagsTestCase
+from test_version                       import VersionTestCase
+
+if __name__ == '__main__':
+    unittest.main()
