d4bf98c146360793ab6f91cdf970fb088a38f887
Brad Figg <brad.figg@canonical.com>
2015-09-02 21:20:16 -0700

    [error.py] Added MaasApiHttpConflict and MaasApiNodeStateReady
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- maasapi/error.py
+++ maasapi/error.py
@@ -23,9 +23,21 @@ class MaasApiNotImplemented(Exception):
         s.status  = 0
         s.message = "This method/property has not been implemented."
 
+# MaasApiStandardException
+#
+class MaasApiStandardException(Exception):
+    '''
+    Generic base, exception class.
+    '''
+    # __init__
+    #
+    def __init__(s, status, msg):
+        s.status  = status
+        s.message = msg
+
 # MaasApiUnknownError
 #
-class MaasApiUnknownError(Exception):
+class MaasApiUnknownError(MaasApiStandardException):
     '''
     Place holder exception until we determine a more specific exception to
     throw.
@@ -33,24 +45,22 @@ class MaasApiUnknownError(Exception):
     # __init__
     #
     def __init__(s, status, msg):
-        s.status  = status
-        s.message = msg
+        super(MaasApiStandardException, s).__init__(status, msg)
 
 # MaasApiCertificateVerificationError
 #
-class MaasApiCertificateVerificationError(Exception):
+class MaasApiCertificateVerificationError(MaasApiStandardException):
     '''
     The MAAS credentials that are specified are not valid for this MAAS server.
     '''
     # __init__
     #
     def __init__(s, status, msg):
-        s.status  = status
-        s.message = msg
+        super(MaasApiStandardException, s).__init__(status, msg)
 
 # MaasApiPowerResponseTimeout
 #
-class MaasApiPowerResponseTimeout(Exception):
+class MaasApiPowerResponseTimeout(MaasApiStandardException):
     '''
     While attempting to get the power state for a node the response back was
     an HTTP 503 with 'Timed out waiting for power response'. It's possible
@@ -59,18 +69,39 @@ class MaasApiPowerResponseTimeout(Exception):
     # __init__
     #
     def __init__(s, status, msg):
-        s.status  = status
-        s.message = msg
+        super(MaasApiStandardException, s).__init__(status, msg)
+
+# MaasApiNodeStateSteady
+#
+class MaasApiNodeStateSteady(MaasApiStandardException):
+    '''
+    A node abort failed. The node was in a Ready state, not performing any
+    operation that could be aborted.
+    '''
+    # __init__
+    #
+    def __init__(s, status, msg):
+        super(MaasApiStandardException, s).__init__(status, msg)
 
 # MaasApiHttpServiceUnavailable
 #
-class MaasApiHttpServiceUnavailable(Exception):
+class MaasApiHttpServiceUnavailable(MaasApiStandardException):
     '''
     While communicating with the MAAS rest interface we got back an HTTP 503.
     '''
     # __init__
     #
     def __init__(s, status, msg):
-        s.status  = status
-        s.message = msg
+        super(MaasApiStandardException, s).__init__(status, msg)
+
+# MaasApiHttpConflict
+#
+class MaasApiHttpConflict(MaasApiStandardException):
+    '''
+    While communicating with the MAAS rest interface we got back an HTTP 409.
+    '''
+    # __init__
+    #
+    def __init__(s, status, msg):
+        super(MaasApiStandardException, s).__init__(status, msg)
 
