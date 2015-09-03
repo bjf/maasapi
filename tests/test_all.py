f927079753c3f5cc5ce4625b93d714681dfb2b0d
Brad Figg <brad.figg@canonical.com>
2015-09-02 19:43:20 -0700

    [test_all.py] Add Tag, Zone and Zones test cases
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,7 +1,10 @@
 
 import unittest
 from test_tags                          import TagsTestCase
+from test_tag                           import TagTestCase
 from test_version                       import VersionTestCase
+from test_zones                         import ZonesTestCase
+from test_zone                          import ZoneTestCase
 
 if __name__ == '__main__':
     unittest.main()
