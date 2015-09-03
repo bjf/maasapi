92ccfe3061d8a31dc0431d7fa5aab73bae2ca0d7
Brad Figg <brad.figg@canonical.com>
2015-09-02 19:37:53 -0700

    [nodes.py] Make the uri used for fetching nodes more flexible, one that can be sent in as an argument.
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- maasapi/nodes.py
+++ maasapi/nodes.py
@@ -13,11 +13,12 @@ class Nodes(object):
 
     # __init__
     #
-    def __init__(s, maas, op='list'):
+    def __init__(s, maas, op='list', uri=u'/nodes/'):
         center(s.__class__.__name__)
         s.__maas = maas
         s.__resources = None
         s.__op = op
+        s.__uri = uri
         cleave(s.__class__.__name__)
 
     # __len__
@@ -49,7 +50,7 @@ class Nodes(object):
     def __fetch_if_needed(s):
         center(s.__class__.__name__)
         if s.__resources is None:
-            response = s.__maas.get(u'/nodes/', op=s.__op)
+            response = s.__maas.get(s.__uri, op=s.__op)
             if not response.ok:
                 if type(response.data) == str:
                     cleave(s.__class__.__name__)
